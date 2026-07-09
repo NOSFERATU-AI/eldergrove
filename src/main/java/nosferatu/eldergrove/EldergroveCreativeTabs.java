6603e1c9ee0fb5062674259d0f568fac1016b93e
NOSFERATU-AI <nosferatu.ai01@gmail.com>
2026-07-09 23:57:13 +0200

    Add Eldergrove grass to creative tab

--- src/main/java/nosferatu/eldergrove/EldergroveCreativeTabs.java
+++ src/main/java/nosferatu/eldergrove/EldergroveCreativeTabs.java
@@ -21,6 +21,7 @@ public final class EldergroveCreativeTabs {
                     .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
                     .icon(() -> EldergroveItems.GROVE_HEART.get().getDefaultInstance())
                     .displayItems((parameters, output) -> {
+                        output.accept(EldergroveItems.ELDERGROVE_GRASS.get());
                         output.accept(EldergroveItems.ELDERGROVE_MOSS.get());
                         output.accept(EldergroveItems.ELDERWOOD_LOG.get());
                         output.accept(EldergroveItems.ELDERWOOD_PLANKS.get());
